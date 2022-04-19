67af0d98c1da470292099bc5c48fa8e2108f7431
Ronaldo Cysne <cysne@localhost.localdomain>
2022-04-18 21:48:00 -0300

    Telas tutorial app

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Report from "./pages/Report";
 import Home from "./pages/Home";
 import Exemplo from "./pages/Exemplo";
 import NotFound from "./pages/NotFound";
+import Tutorial from "./pages/Tutorial"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/tutorial" element={<Tutorial/>}/>
         <Route path="/relatorio" element={<Report/>}/>
         <Route path="/exemplo" element={<Exemplo/>}/>
 
